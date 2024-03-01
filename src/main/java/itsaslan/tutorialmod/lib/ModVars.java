fbca034c69fc539f9917130ae5cccb967538a346
ItsAslan <161654129+ItsAslan@users.noreply.github.com>
2024-03-01 14:59:03 -0500

    Update ModVars.java
    hehehaw

--- src/main/java/itsaslan/tutorialmod/lib/ModVars.java
+++ src/main/java/itsaslan/tutorialmod/lib/ModVars.java
@@ -6,7 +6,7 @@ public class ModVars {
     public static final String MOD_Name = "Tutorial Mod";
 
     //Mod Version
-    public static final String MOD_Version = "0.0.1";
+    public static final String MOD_Version = "0.0.2";
 
     //Mod ID
     public static final String MOD_ID = "tutorialmod";
